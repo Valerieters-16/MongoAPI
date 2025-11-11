a1152941bbd82b683b352097619c662fcedf3011
Valerie Ters <valerieters@hotmail.com>
2025-11-11 19:17:42 +0300

    first commit

---
+++ src/db.js
@@ -0,0 +1,25 @@
+ import { MongoClient, ServerApiVersion } from "mongodb";
+ import dotenv from "dotenv";
+ dotenv.config();
+
+ const client = new MongoClient(process.env.MONGO_URL, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+ });
+
+ let _db;
+
+ export async function connectDB() {
+    if (_db) return _db; 
+    await client.connect(); // Connect
+    _db = client.db(process.env.DB_NAME); // Use
+    return _db;
+ }
+
+ export function getDB(){
+    if(!_db) throw new Error("DB not initialized. Call connectDB() first");
+    return _db;
+ }
