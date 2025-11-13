5fc6d871f096d8dfc86e05b9112f6c4449bf386f
Valerie Ters <valerieters@hotmail.com>
2025-11-13 13:30:06 +0300

    changes-03

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("docs"));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
